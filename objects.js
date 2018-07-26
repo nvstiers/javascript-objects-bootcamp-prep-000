e06ddee8f50af63c2a5d23ed851be556582c9fe7
Nathaniel Stiers <nvstiers@gmail.com>
2018-07-26 08:27:47 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'faces':'Ooh La La'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName]= songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName, songTitle){
+  delete meals.artistName;
+  return playlist
+}
